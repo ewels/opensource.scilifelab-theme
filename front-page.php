db63399367b3d74ad2dba91f82359bc97d517451
Robin Andeer <robin.andeer@gmail.com>
2015-02-26 08:23:36 +0100

    change label of developer link on front page

--- front-page.php
+++ front-page.php
@@ -57,7 +57,7 @@
 </section>
 
 <nav>
-    <a class="button" href="<?php echo home_url('authors'); ?>" title="Authors">See all project authors</a>
+    <a class="button" href="<?php echo home_url('authors'); ?>" title="Authors">See all developers</a>
 </nav>
 
 <?php if(!is_user_logged_in()){ ?>
